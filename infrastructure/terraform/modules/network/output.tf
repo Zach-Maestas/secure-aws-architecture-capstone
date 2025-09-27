5575552b0c217f4ebbc19abb3d27e8d3da2d5320
Zach-Maestas <zachary.maestas03@gmail.com>
2025-09-26 18:17:34 -0600

    feat(network): setup Internet Gateways and Route Tables

--- infrastructure/terraform/modules/network/output.tf
+++ infrastructure/terraform/modules/network/output.tf
@@ -2,6 +2,15 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
+output "public_rt_id" {
+  value = aws_route_table.public.id
+}
+
+output "private_rt_ids" {
+  description = "List of private route table IDs (one per AZ)"
+  value       = aws_route_table.private[*].id
+}
+
 output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,42 +1,31 @@
 variable "project" {
-  default = "secure-aws-architecture-capstone"
-}
-
-variable "vpc_cidr" {
-  default = "10.0.0.0/16"
-}
-variable "project" {
   description = "Project name"
-  default     = "secure-aws-architecture-capstone"
+  type        = string
 }
 
 variable "vpc_cidr" {
-  description = "CIDR block for the VPC"
-  default     = "10.0.0.0/16"
+  description = "CIDR for the VPC"
+  type        = string
 }
 
-variable "azs" {
-  description = "Availability Zones"
-  type        = list(string)
-  default     = ["us-west-2a", "us-west-2b"]
+variable "region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-west-2"
 }
 
 variable "public_subnet_cidrs" {
-  description = "List of public subnet CIDRs (1 per AZ)"
+  description = "List of public subnet CIDRs"
   type        = list(string)
-  default     = [
-    "10.0.0.0/24", # AZ A
-    "10.0.1.0/24"  # AZ B
-  ]
 }
 
 variable "private_subnet_cidrs" {
-  description = "List of private subnet CIDRs (2 per AZ)"
+  description = "List of private subnet CIDRs"
   type        = list(string)
-  default     = [
-    "10.0.2.0/24", # AZ A (EC2)
-    "10.0.3.0/24", # AZ B (EC2)
-    "10.0.4.0/24", # AZ A (RDS)
-    "10.0.5.0/24"  # AZ B (RDS)
-  ]
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-west-2a", "us-west-2b"]
 }
