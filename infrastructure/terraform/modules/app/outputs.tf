bc0789391f900d882889689239ce4ddbf80dc569
Zach-Maestas <zachary.maestas03@gmail.com>
2025-10-21 00:04:48 -0600

    feat(data): added RDS with multi-az enabled

--- infrastructure/terraform/modules/app/outputs.tf
+++ infrastructure/terraform/modules/app/outputs.tf
@@ -1,15 +1,19 @@
 output "alb_sg_id" {
+  description = "Security group ID for the ALB"
   value = aws_security_group.alb.id
 }
 
 output "ec2_sg_id" {
+  description = "Security group ID for the EC2 instances"
   value = aws_security_group.ec2.id
 }
 
 output "alb_dns_name" {
+  description = "DNS name of the ALB"
   value = aws_lb.this.dns_name
 }
 
 output "target_group_arn" {
+  description = "ARN of the target group"
   value = aws_lb_target_group.app.arn
 }
